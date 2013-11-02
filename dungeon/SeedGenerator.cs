dc25fd5ae61b173d148307a89c678c95cdc4064d
Marek Mottl <marek@czechgeeks.cz>
2013-11-02 12:02:53 +0100

    init commit with seed generator

---
+++ dungeon/SeedGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dungeon
+{
+    public static class SeedGenerator : Math
+    {
+        public static int Seed { get; set; }
+
+        public static int Random(int min=0,int max=1)
+        {
+            Seed = (Seed * 9301 + 49297) % 233280;
+            int rnd = Seed / 233280;
+
+            return min + rnd * (max - min);
+        }
+    }
+}
